0b98c966c91f640a1ad22292a074409d874d8b65
qianlingzhi <qianlingzhi1996@163.com>
2018-11-27 16:33:30 +0800

    钱灵志第二次提交页面,完成查询商品详情功能

--- js/productList.js
+++ js/productList.js
@@ -1,13 +1,25 @@
 $(function () {
   let pageId = 1;
-  let categotyId = 0;
+  let categotyId = getQueryString("categoryId")||0;
   var productList =new Product(categotyId,pageId);
   productList.getProductList();
+  productList.getCategory()
   productList.getNext();
   productList.getLast()
   mui('.mui-scroll-wrapper').scroll({
     deceleration: 0.0005 //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
   });
+  //进入商品详情
+  $(".product-list-content").on("tap","li" ,function () {
+    let productId = $(this).find(".product-id").data("id");
+    location.href = "productDetail.html?productId="+productId;
+  })
+  //三级菜单点击效果
+  $(".reback .categoty").on("tap",function () {
+    let categoryId = $(this).data("id");
+    console.log(categoryId);
+    location.href = "productList.html?categoryId="+categoryId;
+  })
 })
 
 class Product {
@@ -24,9 +36,22 @@ class Product {
       dataType: "json",
       data: {categoryid: this.categotyId, pageid: this.pageId},
       success(data) {
+        console.log(data);
         var productHtml = template("productListTpl", data);
         $(".product-list-content ul ").html(productHtml);
         that.maxPage = Math.ceil(data.totalCount/data.pagesize) ;
+      }
+    })
+  }
+  getCategory(){
+    let that =this;
+    $.ajax({
+      url: "http://localhost:9090/api/getcategorybyid",
+      type: "get",
+      dataType: "json",
+      data: {categoryid: that.categotyId},
+      success(data) {
+        $(".reback .categoty").html(data.result[0].category).data("id",data.result[0].categoryId);
       }
     })
   }
@@ -57,6 +82,10 @@ class Product {
     })
   }
 }
-  
+function getQueryString(name) {
+  var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
+  var r = window.location.search.substr(1).match(reg);
+  if (r != null) return unescape(r[2]); return null;
+}
 
 
