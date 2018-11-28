1a742939e98a3fb63bed2ae2d47e4e7a1ef6fe25
paohuoce <811258575@qq.com>
2018-11-28 17:30:25 +0800

    ‘第二次提交解决mui的a标签无法跳转

--- js/category.js
+++ js/category.js
@@ -7,6 +7,11 @@ $(function(){
         indicators: false,
         deceleration: 0.0005 //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006});
     });
+//mui框架a标签无法跳转的委托事件
+    mui('#main').on('tap','a',function(){
+        console.log(this);
+        window.top.location.href=this.href;
+    });
 
 });
 
